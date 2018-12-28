ae3c24107f40b435426813731eeed67ff0fdf48e
Karol-Szykula <karol.szykula@wp.pl>
2018-12-28 12:55:08 +0100

    Request method_ovveride example

--- server.js
+++ server.js
@@ -2,15 +2,34 @@ const http = require('http')
 const connect = require('connect')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
+const methodOverride = ('method-override')
 
 const app = connect()
-    .use(logger())
-    .use(bodyParser.urlencoded({
-        extended: true
-    }))
-    .use(bodyParser.json())
-    .use(function (req, res) {
-        res.end(JSON.stringify(req.body))
-    })
 
-http.createServer(app).listen(4000)
+    .use(logger('dev'))
+
+    // .use(bodyParser.urlencoded({
+    //     extended: true
+    // }))
+
+    .use(edit)
+    .use(update)
+
+http.createServer(app).listen(3000)
+
+
+function edit(req, res, next) {
+    if ('GET' !== req.method) return next()
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<form method="post">')
+    res.write('<input type="hidden" name="_method" value="put" />')
+    res.write('<input type="text" name="user[name]" value="Tobi" />');
+    res.write('<input type="submit" value="Zmień" />')
+    res.write('</form>')
+    res.end()
+}
+
+function update(req, res, next) {
+    if ('PUT' != req.method) return next()
+    res.end('User name has been changed to: ' + req.body.user.name)
+}
