691331078d3a0df453e8775c9e28a598f1324889
Karol-Szykula <karol.szykula@wp.pl>
2018-12-28 09:57:42 +0100

    Example of simple parse of url

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 const http = require('http')
 const connect = require('connect')
-const bodyParser = require('body-parser')
+const qs = require('qs')
 
 const app = connect()
-    .use(bodyParser({
-        limit: '64kb',
-        type: 'application/x-www-form-urlencoded'
-    }))
-    .use(bodyParser({
-        limit: '32kb',
-        type: 'application/json'
-    }))
-    .use(bodyParser({
-        limit: '2mb',
-        type: 'image'
-    }))
-    .use(bodyParser({
-        limit: '300mb',
-        type: 'video'
-    }))
-    .use(hello)
+    .use(function (req, res, next) {
+        res.setHeader('Content-Type', 'application/json')
+        console.log(req.url)
+        res.end(JSON.stringify(req.url))
+    })
 
 http.createServer(app).listen(3000)
 
-function hello(req, res, next) {
-    console.log('Hello')
+function query(req, res, next) {
+    
 }
